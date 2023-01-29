3de9e70e645cb8931d701699a24fba46bad8ceea
Arjun Thakur <arjthak@amazon.com>
2023-01-29 17:41:44 +0530

    added git.sh script file

---
+++ src/scripts/git.sh
@@ -0,0 +1,12 @@
+#just run this file
+# sh git.sh
+# git.sh is the filename
+cd /Users/arjthak/git/DataStructuresAndAlgorithmsWithJava/src
+git checkout master
+git pull
+git add .
+echo 'Enter the commit message:'
+read commitMessage
+git commit -m "$commitMessage"
+git push origin master
+echo 'Pushed!'
