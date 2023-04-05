9e4bdd7f5c1a802a830a791cdb98bfa225758a16
Arjun Thakur <arjthak@amazon.com>
2023-04-05 22:03:23 +0530

    organised imports of pub-sub

---
+++ src/classic/comp/sci/problems/producerconsumerwithqueue/interfaces/Consumer.java
@@ -0,0 +1,11 @@
+package classic.comp.sci.problems.producerconsumerwithqueue.interfaces;
+
+public interface Consumer {
+    void consume();
+
+    void register();
+
+    String getGroupId();
+
+
+}
