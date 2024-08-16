8bbaa8da4aa3318a86d812fc61ba728a7b4e425a
Arjun Thakur <thakurarjun247@gmail.com>
2024-08-16 16:40:53 +0530

    added redo and undo to command pattern

--- src/designpatterns/structural/decorator/CondimentDecorator.java
+++ src/designpatterns/structural/decorator/CondimentDecorator.java
@@ -1,5 +1,35 @@
 package designpatterns.structural.decorator;
 
+abstract class Beverage {
+    String description;
+    String getDescription(){
+        return  description;
+    }
+    public abstract double getCost();
+}
+
+class Espresso extends Beverage{
+    @Override
+    String getDescription() {
+        return "Espresso";
+    }
+    @Override
+    public double getCost(){
+        return 60;
+    }
+}
+
+class HouseBlend extends Beverage{
+    @Override
+    String getDescription() {
+        return "HouseBlend";
+    }
+    @Override
+    public double getCost(){
+        return 80;
+    }
+}
+
 public abstract class CondimentDecorator  extends Beverage{
     abstract String getDescription() ;
 }
@@ -35,5 +65,15 @@ class Sugar extends CondimentDecorator{
     public double getCost(){
         //double d=super.cost();
         return 3 + beverage.getCost();
+    }
+}
+
+class Client {
+    public static void main(String[] args) {
+        Beverage beverage=new HouseBlend();
+        beverage=new SteamedMilk(beverage);
+        beverage=new Sugar(beverage);
+
+        System.out.println(beverage.getDescription() + beverage.getCost());
     }
 }
